6d6367cb8d96534e9341059196857cc862843285
MARIO CONSTANTINO <mconstantinojr@gmail.com>
2022-12-01 21:57:53 -0300

    [mario] modelo de documentos e contratos

--- api/src/main/java/com/atos/inventario/model/Contrato.java
+++ api/src/main/java/com/atos/inventario/model/Contrato.java
@@ -1,5 +1,86 @@
 package com.atos.inventario.model;
 
-public class Contrato extends Documento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_ARQUIVO_CONTRATO")
+public class Contrato extends Documento implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idArquivoContrato;
+	
+	private String documentoEncaminhamento;
+	private String numeroCaixaEscritorioOrigem;
+	private String numeroCaixaArquivoCustodia;
+	private String numeroContrato;
+	private String numeroPec;
+	private String empresaContratada;
+	private String objetoResumido;
+	
+	public long getIdArquivoContrato() {
+		return idArquivoContrato;
+	}
+	public void setIdArquivoContrato(long idArquivoContrato) {
+		this.idArquivoContrato = idArquivoContrato;
+	}
+	public String getDocumentoEncaminhamento() {
+		return documentoEncaminhamento;
+	}
+	public void setDocumentoEncaminhamento(String documentoEncaminhamento) {
+		this.documentoEncaminhamento = documentoEncaminhamento;
+	}
+	public String getNumeroCaixaEscritorioOrigem() {
+		return numeroCaixaEscritorioOrigem;
+	}
+	public void setNumeroCaixaEscritorioOrigem(String numeroCaixaEscritorioOrigem) {
+		this.numeroCaixaEscritorioOrigem = numeroCaixaEscritorioOrigem;
+	}
+	public String getNumeroCaixaArquivoCustodia() {
+		return numeroCaixaArquivoCustodia;
+	}
+	public void setNumeroCaixaArquivoCustodia(String numeroCaixaArquivoCustodia) {
+		this.numeroCaixaArquivoCustodia = numeroCaixaArquivoCustodia;
+	}
+	public String getNumeroContrato() {
+		return numeroContrato;
+	}
+	public void setNumeroContrato(String numeroContrato) {
+		this.numeroContrato = numeroContrato;
+	}
+	public String getNumeroPec() {
+		return numeroPec;
+	}
+	public void setNumeroPec(String numeroPec) {
+		this.numeroPec = numeroPec;
+	}
+	public String getEmpresaContratada() {
+		return empresaContratada;
+	}
+	public void setEmpresaContratada(String empresaContratada) {
+		this.empresaContratada = empresaContratada;
+	}
+	public String getObjetoResumido() {
+		return objetoResumido;
+	}
+	public void setObjetoResumido(String objetoResumido) {
+		this.objetoResumido = objetoResumido;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+	
+	
+	
 }
--- api/src/main/java/com/atos/inventario/model/Documento.java
+++ api/src/main/java/com/atos/inventario/model/Documento.java
@@ -1,5 +1,58 @@
 package com.atos.inventario.model;
 
-public abstract class Documento {
+import java.util.Date;
 
+import javax.persistence.ManyToOne;
+
+public abstract class Documento {
+	
+	@ManyToOne
+	private UnidadeProdutora unidadeProdutora;
+	@ManyToOne
+	private ClassificacaoDocumental classificacaoDocumental;
+	private Date dataLimite;
+	@ManyToOne
+	private Empregado empregado;
+	private Date dataCriacao;
+	@ManyToOne
+	private Localizacao localizacao;
+	
+	public UnidadeProdutora getUnidadeProdutora() {
+		return unidadeProdutora;
+	}
+	public void setUnidadeProdutora(UnidadeProdutora unidadeProdutora) {
+		this.unidadeProdutora = unidadeProdutora;
+	}
+	public ClassificacaoDocumental getClassificacaoDocumental() {
+		return classificacaoDocumental;
+	}
+	public void setClassificacaoDocumental(ClassificacaoDocumental classificacaoDocumental) {
+		this.classificacaoDocumental = classificacaoDocumental;
+	}
+	public Date getDataLimite() {
+		return dataLimite;
+	}
+	public void setDataLimite(Date dataLimite) {
+		this.dataLimite = dataLimite;
+	}
+	public Empregado getEmpregado() {
+		return empregado;
+	}
+	public void setEmpregado(Empregado empregado) {
+		this.empregado = empregado;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
+	}
+	
+	
 }
