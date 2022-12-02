650308abd9e7171a47adde2e67a88a1ad67fec03
MarcosBarbosa <marcsalexandr@gmail.com>
2022-12-01 22:20:25 -0300

    [MARCOS] ENTIDADES DOCUMENTOS

---
+++ api/src/main/java/com/atos/inventario/model/Financeira.java
@@ -0,0 +1,78 @@
+package com.atos.inventario.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_FINANCEIRA")
+public class Financeira extends Documento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String documentoEncaminhamento;
+	private String numeroCaixaEscritorioOrigem;
+	private String numeroCaixaArquivoCustodia;
+	private Date   dataPagamento;
+	private String unidadePagamento;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDocumentoEncaminhamento() {
+		return documentoEncaminhamento;
+	}
+
+	public void setDocumentoEncaminhamento(String documentoEncaminhamento) {
+		this.documentoEncaminhamento = documentoEncaminhamento;
+	}
+
+	public String getNumeroCaixaEscritorioOrigem() {
+		return numeroCaixaEscritorioOrigem;
+	}
+
+	public void setNumeroCaixaEscritorioOrigem(String numeroCaixaEscritorioOrigem) {
+		this.numeroCaixaEscritorioOrigem = numeroCaixaEscritorioOrigem;
+	}
+
+	public String getNumeroCaixaArquivoCustodia() {
+		return numeroCaixaArquivoCustodia;
+	}
+
+	public void setNumeroCaixaArquivoCustodia(String numeroCaixaArquivoCustodia) {
+		this.numeroCaixaArquivoCustodia = numeroCaixaArquivoCustodia;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public String getUnidadePagamento() {
+		return unidadePagamento;
+	}
+
+	public void setUnidadePagamento(String unidadePagamento) {
+		this.unidadePagamento = unidadePagamento;
+	}
+
+	
+
+}
---
+++ api/src/main/java/com/atos/inventario/model/Licitacao.java
@@ -0,0 +1,86 @@
+package com.atos.inventario.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_LICITACAO")
+public class Licitacao extends Documento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String documentoEncaminhamento;
+	private String numeroCaixaEscritorioOrigem;
+	private String numeroCaixaArquivoCustodia;
+	private String numeroProcessoLicitatorio;
+	private String numeroPec;
+	private String objetoResumido;
+	
+
+	
+	public String getNumeroProcessoLicitatorio() {
+		return numeroProcessoLicitatorio;
+	}
+
+	public void setNumeroProcessoLicitatorio(String numeroProcessoLicitatorio) {
+		this.numeroProcessoLicitatorio = numeroProcessoLicitatorio;
+	}
+
+	public String getObjetoResumido() {
+		return objetoResumido;
+	}
+
+	public void setObjetoResumido(String objetoResumido) {
+		this.objetoResumido = objetoResumido;
+	}
+
+	public String getNumeroPec() {
+		return numeroPec;
+	}
+
+	public void setNumeroPec(String numeroPec) {
+		this.numeroPec = numeroPec;
+	}
+
+	public String getDocumentoEncaminhamento() {
+		return documentoEncaminhamento;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setDocumentoEncaminhamento(String documentoEncaminhamento) {
+		this.documentoEncaminhamento = documentoEncaminhamento;
+	}
+
+	public String getNumeroCaixaEscritorioOrigem() {
+		return numeroCaixaEscritorioOrigem;
+	}
+
+	public void setNumeroCaixaEscritorioOrigem(String numeroCaixaEscritorioOrigem) {
+		this.numeroCaixaEscritorioOrigem = numeroCaixaEscritorioOrigem;
+	}
+
+	public String getNumeroCaixaArquivoCustodia() {
+		return numeroCaixaArquivoCustodia;
+	}
+
+	public void setNumeroCaixaArquivoCustodia(String numeroCaixaArquivoCustodia) {
+		this.numeroCaixaArquivoCustodia = numeroCaixaArquivoCustodia;
+	}
+
+}
